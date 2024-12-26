8740062105835daed590a475924bed753fcf1111
kaa-ceng <knkarakut@gmail.com>
2024-12-26 04:04:54 +0300

    parametr and constant update

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -16,23 +16,25 @@ Limit: 1
         self.random = random
         
         pass
-    def send(self, log_file_name, parameter1, parameter2):
+    def send(self, log_file_name, dst_ip, dst_port, min_packets, max_packets, delay_1_min, delay_1_max, delay_0_min, delay_0_max):
+
         """
         - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
         - After the implementation, please rewrite this comment part to explain your code basically.
         """
         "creates random binary message and then goes through each bit of this message. For each bit it generates a random number of packets and sends them to receiever."
         "whether bit is 0 or 1 delay is changed of the burst therefore on receiver side we can understand if its 0 or 1 based on the delay"
+        
         binary_message = self.generate_random_binary_message_with_logging(log_file_name)
         for bit in binary_message:
-            num_packets = self.random.randint(2, 6)
+            num_packets = self.random.randint(min_packets, max_packets)
             for i in range(num_packets):
-                packet = IP(dst="172.18.0.3")/TCP(dport=8000)
-                self.send(packet)
-            if(bit == '1'):
-                self.sleep_random_time_ms(10,20)
-            elif(bit == '0'):
-                self.sleep_random_time_ms(20,50)
+                packet = IP(dst=dst_ip)/TCP(dport=dst_port)
+                send(packet)
+            if bit == '1':
+                self.sleep_random_time_ms(delay_1_min, delay_1_max)
+            elif bit == '0':
+                self.sleep_random_time_ms(delay_0_min, delay_0_max)
         
      
              

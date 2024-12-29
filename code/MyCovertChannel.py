81bf20187afa11af3c594b51a0a3006c15080480
sighaunt3 <sighaunt@gmail.com>
2024-12-29 14:04:14 +0300

    final

--- code/MyCovertChannel.py
+++ code/MyCovertChannel.py
@@ -6,9 +6,6 @@ class MyCovertChannel(CovertChannelBase):
     
     """
     Covert Timing Channel that exploits Idle Period Between Packet Bursts using TCP [Code: CTC-IPPB-TCP]
-
-    - You are not allowed to change the file name and class name.
-    - You can edit the class in any way you want (e.g. adding helper functions); however, there must be a "send" and a "receive" function, the covert channel will be triggered by calling these functions.
     """
     def __init__(self):
         super().__init__()
@@ -18,14 +15,15 @@ class MyCovertChannel(CovertChannelBase):
         
     def send(self, log_file_name, src_ip, dst_ip, dst_port, min_packets, max_packets, delay_1_min, delay_1_max, delay_0_min, delay_0_max,zero,one):
         """
-        - In this function, you expected to create a random message (using function/s in CovertChannelBase), and send it to the receiver container. Entire sending operations should be handled in this function.
-        - After the implementation, please rewrite this comment part to explain your code basically.
-        """
-        "creates random binary message and then goes through each bit of this message. For each bit it generates a random number of packets and sends them to receiver."
-        "whether bit is 0 or 1 delay is changed of the burst therefore on receiver side we can understand if its 0 or 1 based on the delay"
-        "generates random number of packets for each bit and stores that many packets in the burst packets list once every packet is added to burst packet it sends it all together"
+        This function creates a random binary message and sends it to the receiver container.
+        It generates a random binary message using a function from CovertChannelBase.
+        For each bit in the message, it generates a random number of packets and stores them in a list called num_packets. Once every all the packets have been created for that
+        bit the list is sent to the receiver all together in one burst.
+        The delay between bursts of packets is adjusted based on whether the bit is '0' or '1'.
+        The function logs the binary message.
+    """
         
-        binary_message = self.generate_random_binary_message_with_logging("sender.log",16,16)
+        binary_message = self.generate_random_binary_message_with_logging("sender.log")
      
         start_time = time.time()
 
@@ -52,10 +50,10 @@ class MyCovertChannel(CovertChannelBase):
     def receive(self, log_file_name, port, threshold_0_min, threshold_0_max, threshold_1_min, threshold_1_max,src_ip,dst_ip,dot,zero,one):
         """
     This function receives and decodes a covert message transmitted over a network using timing differences between packets.
-    - It captures packets on a specified port and processes them to determine if they represent a '0' or '1' based on timing thresholds.
-    - The function appends the decoded bits to a list and converts every 8 bits to a character.
-    - The process continues until a '.' character is detected, indicating the end of the message.
-    - The decoded message is then logged to a specified log file.
+    It captures packets on a specified port and processes them to determine if they represent a '0' or '1' based on timing thresholds.
+    The function appends the decoded bits to a list and converts every 8 bits to a character.
+    The process continues until a '.' character is detected, indicating the end of the message.
+    The decoded message is then logged to a specified log file.
     """
         packets = []
         global is_dot
